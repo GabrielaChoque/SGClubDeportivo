acff30ef0d7a5e8b7641c58e3528dd03a6a87731
elLCCH <cristian.98oruro@gmail.com>
2024-01-20 17:38:14 -0400

    Funcionacion

--- SGClubDeportivo/Properties/Settings.Designer.cs
+++ SGClubDeportivo/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SGClubDeportivo.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace SGClubDeportivo.Properties {
         public string BdClubDeportivoConnectionString {
             get {
                 return ((string)(this["BdClubDeportivoConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LCCH;Initial Catalog=BdClubDeportivo;User ID=sa;Password=12345678;Tru" +
+            "stServerCertificate=True")]
+        public string BdClubDeportivoConnectionString1 {
+            get {
+                return ((string)(this["BdClubDeportivoConnectionString1"]));
             }
         }
     }
